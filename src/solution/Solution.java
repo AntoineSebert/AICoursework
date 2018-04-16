f4f9554e92590cd2930ea3bc41a32306c43b46da
Antoine <antoine.sb@orange.fr>
2018-04-16 15:38:38 +0100

    Finishing the State class + creation of PromptValues()

--- src/solution/Solution.java
+++ src/solution/Solution.java
@@ -1,11 +1,35 @@
 package solution;
 
+import java.util.Scanner;
+
 import search.*;
 import search.informed.*;
 //import org.apache.commons.math3.util.*;
 
 public class Solution {
+	static public int RAFT_SIZE = 2;
+	static public int RAFT_MAX_WEIGHT = 180;
+	static public int NUM_PEOPLE = 4;
+	static public int PEOPLE[] = { /* Adam (100.0) Betty (90.0) Claire (50.0) Dave (30.0) */ };
+	
 	public static void main(String[] args) {
-		
+		promptValues();
+	}
+	
+	private static void promptValues() {
+		Scanner scan = new Scanner(System.in);
+
+		System.out.println("RAFT_SIZE");
+		RAFT_SIZE = scan.nextInt();
+
+		System.out.println("RAFT_MAX_WEIGHT");
+		RAFT_MAX_WEIGHT = scan.nextInt();
+
+		System.out.println("NUM_PEOPLE");
+		NUM_PEOPLE = scan.nextInt();
+		for(int i = 0; i < NUM_PEOPLE; i++)
+			PEOPLE[i] = new Person(scan.nextInt());
+
+		scan.close();
 	}
 }
--- src/solution/State.java
+++ src/solution/State.java
@@ -2,21 +2,17 @@ package solution;
 
 import java.util.ArrayList;
 import java.util.List;
-
-import lab02.mc.McAction;
-import lab02.mc.MissionariesCannibals;
 import search.ActionStatePair;
 
 public class State implements search.State {
 	/* ATTRIBUTES */
-		private int northPeople, southPeople, totalPeople;
+		private int northPeople, southPeople;
 		private Bank raftLocation;
 	/* MEMBERS */
 		// constructor
 			public State(int np, int sp, Bank location) {
 				northPeople = np;
 				southPeople = sp;
-				totalPeople = np = sp;
 				raftLocation = location;
 			}
 		// operators
@@ -30,53 +26,40 @@ public class State implements search.State {
 				return result;
 			}
 			public boolean equals(State state) {
-				return northPeople == State.northPeople && southPeople == State.southPeople && raftLocation == State.raftLocation;
+				return northPeople == state.getNorthPeople() && southPeople == state.getSouthPeople() && raftLocation == state.getLocation();
 			}
 		// getters
 			public int hashCode() { return northPeople * 10 + southPeople; }
+			public int getNorthPeople() { return northPeople; }
+			public int getSouthPeople() { return southPeople; }
+			public Bank getLocation() { return raftLocation; }
 		// accessors
 			public boolean isInvalid() { return northPeople < 0 || southPeople < 0; }
 			public List<ActionStatePair> successor() {
-				List<ActionStatePair> result = new ArrayList<ActionStatePair>();	//I chose to use an ArrayList object as the list will be short.
+				List<ActionStatePair> result = new ArrayList<ActionStatePair>();	// I chose to use an ArrayList object as the list will be short.
 
-				// This should not happen but just to be safe
-				if (this.isInvalid())	// if current state is invalid
+				if(this.isInvalid())	// if current state is invalid
 					return result;	// return an empty set
+				int numPeopleOnBank = (raftLocation == Bank.NORTH ? northPeople : southPeople);
 				
-				// I changed the code here a bit to make it easier to understand.
-				int numPeopleOnBank = 0;	//the number of M on the current bank
-				int numCannibalsOnBank = 0;		//the number of C
-				
-				// set up values of these 2 variables properly depending on where the raft is
-				numPeopleOnBank = (raftLocation == Bank.NORTH ? northPeople : southPeople);
-				
-				//
-				// The main loops going through all combinations of M and C.
-				// Note that we start from the max value of M and C down to 0.
-				// This makes us generate actions that prefer moving more M and C than fewer.
-				//
-				// For either M or C, the max number we can move is the minimum between the no. of M/C on the current bank and the raft size.
-				//
-				for (int m = Math.min(numPeopleOnBank, MissionariesCannibals.RAFT_SIZE); m >= 0; m--)
-					for (int c = Math.min(numCannibalsOnBank, MissionariesCannibals.RAFT_SIZE); c >= 0; c--) {
-						//
+				// The main loops going through all combinations of M. Note that we start from the max value of M down to 0.
+				// This makes us generate actions that prefer moving more M than fewer.
+				for(int m = Math.min(numPeopleOnBank, MissionariesCannibals.RAFT_SIZE); m >= 0; m--)
 						// You need at least 1 person on the raft, and not more than raft size.
-						// If M+C is within the acceptable range, create an action.
-						//
-						if (m + c <= MissionariesCannibals.RAFT_SIZE && m + c > 0) {
-							Action action = new Action(m, c, oppositeBank(this.raftLocation));
+						// If M is within the acceptable range, create an action.
+						if(m <= MissionariesCannibals.RAFT_SIZE && m > 0) {
+							Action action = new Action(m, oppositeBank(this.raftLocation));
 							State nextState = this.applyAction(action);
-							if (!nextState.isInvalid())	//check for valid combination of M&C on both sides
+							if(!nextState.isInvalid())
 								result.add(new ActionStatePair(action, nextState));
 						}
-					}
 				return result;
 			}
 		// modifiers
 			private Bank oppositeBank(Bank current) { return (current == Bank.NORTH ? Bank.SOUTH : Bank.NORTH); }
 	// modifier ? change the return to "this = "
 	public State applyAction(Action action) {
-		if (action.toBank == Bank.NORTH)
+		if(action.toBank == Bank.NORTH)
 			return new State(northPeople + action.missionaries, southPeople - action.missionaries, Bank.NORTH);
 		return new State(northPeople - action.missionaries, southPeople + action.missionaries, Bank.SOUTH);
 	}
