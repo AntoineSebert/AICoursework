05fc4b7e0386299be9042841c4c4ccc189f597d7
Antoine <antoine.sb@orange.fr>
2018-04-16 19:17:32 +0100

    Modification of successor() + adding getRaftWeight()

--- src/solution/State.java
+++ src/solution/State.java
@@ -7,8 +7,7 @@ import search.ActionStatePair;
 
 public class State implements search.State {
 	/* ATTRIBUTES */
-		//private int northPeople, southPeople;
-		private ArrayList<Integer> northPeople, southPeople;
+		private ArrayList<Integer> northPeople, southPeople, raft;
 		private Bank raftLocation;
 	/* MEMBERS */
 		// constructor
@@ -55,16 +54,22 @@ public class State implements search.State {
 			public ArrayList<Integer> getNorthPeople() { return northPeople; }
 			public ArrayList<Integer> getSouthPeople() { return southPeople; }
 			public Bank getLocation() { return raftLocation; }
+			public int getRaftWeight() {
+				int sum = 0;
+				for(int d : raft)
+					sum += d;
+				return sum;
+			}
 		// accessors
 			public List<ActionStatePair> successor() {
 				List<ActionStatePair> result = new ArrayList<ActionStatePair>();
-				ArrayList<Integer> numPeopleOnBank = (raftLocation == Bank.NORTH ? northPeople : southPeople);
+				ArrayList<Integer> PeopleOnBank = (raftLocation == Bank.NORTH ? northPeople : southPeople);
 				
 				// The main loops going through all combinations of M. Note that we start from the max value of M down to 0.
 				// This makes us generate actions that prefer moving more M than fewer.
-				for(int m = Math.min(numPeopleOnBank, Solution.RAFT_SIZE); m >= 0; m--)
+				for(int m = Math.min(PeopleOnBank.size(), Solution.RAFT_SIZE); m >= 0; m--)
 						// You need at least 1 person on the raft, and not more than raft size. If M is within the acceptable range, create an action.
-						if(0 < m && m <= Solution.RAFT_SIZE) {
+						if(0 < m && m <= Solution.RAFT_SIZE && getRaftWeight() < Solution.RAFT_MAX_WEIGHT) {
 							/*
 							Action action = new Action(m, oppositeBank(this.raftLocation));
 							result.add(new ActionStatePair(action, applyAction(action)));
