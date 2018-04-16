bbdf5f8820017742c4ea5e95e13baadfc79a6a3d
Antoine <antoine.sb@orange.fr>
2018-04-16 14:45:18 +0100

    Adding library, Bank enum, State class

---
+++ src/solution/Bank.java
@@ -0,0 +1,6 @@
+package solution;
+
+public enum Bank {
+	NORTH,
+	SOUTH
+}
--- src/solution/Solution.java
+++ src/solution/Solution.java
@@ -1,6 +1,8 @@
 package solution;
 
 import search.*;
+import search.informed.*;
+//import org.apache.commons.math3.util.*;
 
 public class Solution {
 	public static void main(String[] args) {
