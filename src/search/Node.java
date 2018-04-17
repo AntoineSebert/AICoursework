5c67380fcabfdf26066f4a447fb555d0d7adb06d
Antoine <antoine.sb@orange.fr>
2018-04-17 20:40:20 +0100

    Implementation of heuristic()

--- src/search/Node.java
+++ src/search/Node.java
@@ -42,29 +42,29 @@ public class Node {
 	 * @param parent The parent node leading to this node.
 	 * @param action The action that leads us from the parent to this node.
 	 */
-	public Node(State state,Node parent,Action action) {
+	public Node(State state, Node parent, Action action) {
 		this.state = state;
 		this.parent = parent;
 		this.action = action;
-	} //end method
+	}
 	
 	/**
 	 * Return the cost of the path from the root node to this node.
 	 * @return	The cost of the path (i.e. sum of all actions) from the root to this node.
 	 */
 	public double getCost() {
-		if (this.parent == null)	//this is the root node
+		if(parent == null)	//this is the root node
 			return 0.0;			//0 cost to here
-		return this.action.cost + this.parent.getCost();	//otherwise recursively calculate the cost up to the root
-	} //end method
+		return action.cost + parent.getCost();	//otherwise recursively calculate the cost up to the root
+	}
 	
 	/**
 	 * Return the depth of this node.
 	 * @return	The depth of the node from root. The root node has a depth of 0.
 	 */
 	public int getDepth() {
-		if (this.parent == null)	//this is the root node
+		if (parent == null)	//this is the root node
 			return 0;			//depth is 0
-		return this.parent.getDepth() + 1;
+		return parent.getDepth() + 1;
 	} //end method
 } //end class
--- src/solution/SearchProblem.java
+++ src/solution/SearchProblem.java
@@ -1,7 +1,6 @@
 package solution;
 
 import search.Node;
-import search.State;
 import search.informed.BestFirstSearchProblem;
 
 public class SearchProblem extends BestFirstSearchProblem {
@@ -17,32 +16,14 @@ public class SearchProblem extends BestFirstSearchProblem {
 		public double evaluation(Node node) {
 			return heuristic(node.state);
 		}
-			
 		/**
 		 * This heuristic function estimate how far this state is from a goal.
 		 * @return The remaining distance/cost of the current state to a goal.
-		 */
-		public double heuristic(State currentState) {
-			double result = 0.0;
-			
-			// Explore different ways to estimate the distance of the current state to the goal state.
-			// Note: You can always access the goal state by "this.goalState" as it is stored as an attribute.
-			/*
-			int tiles[][] = ((State)currentState).tiles;
-			int goalTiles[][] = ((State)goalState).tiles;
-			*/
-			// The current implementation is "counting the number of misplaced tiles".
-			// Note that we only look for tiles in counting mismatches, not the space.
-			// Including the space in counting mismatches will over-estimate.
-			/*
-			for (int row = 0; row < 3; row++)
-				for (int col = 0; col < 3; col++)
-					if (tiles[row][col] != 0 && 					//if current tile is not a space
-						tiles[row][col] != goalTiles[row][col])		//and it does not match the one in goal
-						result++;									//increment count by 1
-			*/
-			return result;
+		*/
+		public double heuristic(search.State state) {
+			int maxScore = Solution.sum(goal.getSouthPeople()), currentScore = Solution.sum(((State)state).getSouthPeople());
+			return (double)currentScore / (double)maxScore;
 		}
 		@Override
-		public boolean isGoal(State state) { return state.equals(goal); }
+		public boolean isGoal(search.State state) { return ((State)state).equals(goal); }
 }
